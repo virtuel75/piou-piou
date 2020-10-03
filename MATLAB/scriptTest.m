ff6ea56e8cf0419a220fb559a5df96cdf771ccd5
m-cheicki <mcheicki.dev@gmail.com>
2020-10-03 18:17:51 +0200

    Image to sound - code trouvé sur Internet

--- MATLAB/scriptTest.m
+++ MATLAB/scriptTest.m
@@ -1,8 +1,11 @@
+% Initialization / clean-up code.
+clc;    % Clear the command window.
+close all;  % Close all figures (except those of imtool.)
+clear;  % Erase all existing variables. Or clearvars if you want.
+workspace;  % Make sure the workspace panel is showing.
+
 % Load an image
 img = imread('resources/piou-piou.jpg'); 
 
-% Show image
-% imagesc(img)
-
-% BPSK modulation : 
-bpskmod =  comm.BPSKModulator
+binaryImage = imbinarize(img);
+imshowpair(img, binaryImage, 'montage');
