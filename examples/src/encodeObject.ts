cbbe0408687f9eed38181845ca88a0ca0ddc6009
Cédric Riana Rakotoarisoa <cedricriana@gmail.com>
2020-10-30 18:42:55 +0100

    Add encoding example
    Add typescript scripts to show example of encoding & binarizing object

---
+++ examples/src/encodeObject.ts
@@ -0,0 +1,80 @@
+import base64 from './base64';
+
+// size of a block
+const byteSize: number = 8
+
+let stringifyData = (data: any) => {
+    let result: string = JSON.stringify(data)
+    result = encodeURI(result)
+    return result
+}
+
+let parseData = (str: string) => {
+    let result = decodeURI(str)
+    result = JSON.parse(result)
+    return result
+}
+
+let encodeData = (data: any) => {
+    // tranform data into a stringify data
+    let strData = stringifyData(data)
+
+    // encode data
+    let encodedData = base64.encode(strData)
+
+    // return the data into a base64 string
+    return encodedData
+}
+
+let decodeData = (base64str: string) => {
+    // decode the base64 string
+    let decodedString = base64.decode(base64str)
+
+    // retrieve the data from the decoded string
+    let data = parseData(decodedString)
+
+    return data
+}
+
+let toBinaryArr = (str: string) => {
+    let result: string[] = []
+
+    for (let i = 0; i < str.length; i++) {
+        let bin: string = str[i].charCodeAt(0).toString(2)
+        bin = Array(byteSize - bin.length + 1).join("0") + bin
+        result.push(bin)
+    }
+    return result.join("").split("").map(x => +x)
+}
+
+let fromBinaryArr = (binaryArr: number[]) => {
+    const binaryStr: string = binaryArr.join('')
+    const regex: string = '.{1,' + byteSize + '}'
+    const subBinaryStr: string[] | null = binaryStr.match(new RegExp(regex, 'g'))
+    let result: string[] = []
+
+    for (let i = 0; subBinaryStr && i < subBinaryStr.length; i++) {
+        const tmp: string = subBinaryStr[i]
+        const char: string = String.fromCharCode(parseInt(tmp, 2))
+        result.push(char)
+    }
+
+    return result.join('')
+}
+
+const obj = 'Hello world'
+// encode the object into base64 string
+const encodedData = encodeData(obj)
+// transform the encoded object into binary array
+const binaryData = toBinaryArr(encodedData)
+// transform binary array into base64 string
+const strFromBinary = fromBinaryArr(binaryData)
+// decode the base64 string
+const decodedData = decodeData(strFromBinary)
+
+console.clear()
+console.log('Original obj:', obj)
+console.log('Encoded obj:', encodedData)
+console.log('Binary representation of the encoded obj:', binaryData)
+console.log('Binary array to string:', strFromBinary)
+console.log('Decoded obj:', decodedData)
